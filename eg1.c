725eb8e5cd623fcf90b1257268e3ad221d67fe7a
Ankita-0324 <69087341+Ankita-0324@users.noreply.github.com>
2021-06-19 12:37:50 +0530

    Assignment -17Jun21

--- eg1.c
+++ eg1.c
@@ -1,36 +1,14 @@
-//------------------write a program nested if else------------------
-//#include<stdio.h>
-//void main()
-//{
-/*	int a=24,b=12;
-  /*      int x;
-	x=a+b;*/
-//	printf("%d",x);*/
-//	printf("hello");
-//}
-
-//error in above file
-//egr.c: In function ‘main’:
-//eg1.c:9:14: error: ‘x’ undeclared (first use in this function)
-  //  9 |  printf("%d",x);*/
-   //   |              ^
-//eg1.c:9:14: note: each undeclared identifier is reported only once for each function it appears in
-//eg1.c:9:18: error: expected expression before ‘/’ token
-  //  9 |  printf("%d",x);*/*/
+//--------------------curent time-------------
 
 
-
-//----------------------updated code---------------------
 #include<stdio.h>
+#include<time.h>
+
 void main()
 {
-      #if 0 
-	int a=10,b=24;
-      #if 0 
+	char *time_now;
+	time_t t =time(NULL);
+	time_now=ctime(&t);
 
-	int x;
-	x=a+b;
-        #endif 
-       #endif 
-	printf("hello");
+	printf("data and date now:%s",time_now);
 }
