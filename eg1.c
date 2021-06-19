23631c58d6b5b3005003f040f7cd4877f8920622
Ankita-0324 <69087341+Ankita-0324@users.noreply.github.com>
2021-06-19 12:35:16 +0530

    Assignment 14Jun21

--- eg1.c
+++ eg1.c
@@ -1,22 +1,36 @@
-//-----------------print N to 1 using recursive function----------
+//------------------write a program nested if else------------------
+//#include<stdio.h>
+//void main()
+//{
+/*	int a=24,b=12;
+  /*      int x;
+	x=a+b;*/
+//	printf("%d",x);*/
+//	printf("hello");
+//}
+
+//error in above file
+//egr.c: In function ‘main’:
+//eg1.c:9:14: error: ‘x’ undeclared (first use in this function)
+  //  9 |  printf("%d",x);*/
+   //   |              ^
+//eg1.c:9:14: note: each undeclared identifier is reported only once for each function it appears in
+//eg1.c:9:18: error: expected expression before ‘/’ token
+  //  9 |  printf("%d",x);*/*/
 
 
+
+//----------------------updated code---------------------
 #include<stdio.h>
-void Number(int n)
-{
-printf("%d\n",n);
-
-  n=n-1;
-  
-  if(n==1)
-	  return;
-  Number(n);
-
-}
 void main()
 {
-	int n;
-	printf("Enter No");
-	scanf("%d",&n);
-        Number(n);
+      #if 0 
+	int a=10,b=24;
+      #if 0 
+
+	int x;
+	x=a+b;
+        #endif 
+       #endif 
+	printf("hello");
 }
--- eg2.c
+++ eg2.c
@@ -1,26 +1,7 @@
-//------------print 1 to n number using recursive---------------
-
+//-----------------comment full code-------------
 #include<stdio.h>
-void Number(int n)
-{
-  static int count=1;
-    printf("%d\n",count);
-    count=count+1;
-
-    if(count==n+1)
-	    return;
-
-    Number(n);
-
-
-}
-
-
 void main()
 {
-	int n;
-	printf("Enter tthe number");
-	scanf("%d",&n);
-	Number(n);
+//	printf("hello");
+//	printf("world");
 }
-
